68c2a06afe3b6c4a64e6f2770fc5ea3f7c6257ee
khanhpham-nano <kpham@nanolos.com>
2022-08-15 23:58:53 +0700

    getResponseThroughMongoDBv2

--- getResponseDataThroughMongoDB.ts
+++ getResponseDataThroughMongoDB.ts
@@ -1,5 +1,34 @@
 import { mongoDbTransaction } from './mongodb';
 
+export async function getResponseThroughMongoDBv2(input, request, forceRequest=null) : Promise<ResponseJson> {
+  const tableName = request.name;
+  let response = null;
+  await mongoDbTransaction(async dbo => {
+    const collection = await dbo.collection(tableName).findOne(input);
+    if(collection == null || forceRequest) {
+      response = await request(input);
+      const saveReponse = response ? {
+        msg: response.msg,
+        data: response.data,
+        error: response.error,
+        status: response.status,
+      } : null;
+
+      await dbo.collection(tableName).replaceOne(input, {
+        ...input,
+        response: saveReponse
+      }, {upsert: true});
+
+      response = saveReponse;
+    }
+    else {
+      response = collection.response;
+    }
+  })
+  
+  return response;
+}
+
 export async function getResponseThroughMongoDB(id, request, forceRequest=null) : Promise<ResponseJson> {
   id = id.toString();
   const input = { id };
